dc6cc4f8be499404271b80305a56668359b7d465
jonyspiral <gg@spiralshoes.com>
2026-01-13 02:40:05 -0300

    fix: configurar adaptador cloudflare y output estatico

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-// astro.config.mjs
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  // SUSTITUYE CON TU DOMINIO REAL
   site: 'https://www.cumbresymareas.com.ar',
   integrations: [sitemap()],
+  output: 'static', // Agrega esta línea
+  adapter: cloudflare(),
 });
